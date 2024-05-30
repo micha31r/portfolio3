5af3fd67d50cbcff2396d22def10fe0e9ee927bc
Michael Ren <michaelren0129@gmail.com>
2024-05-30 15:49:50 +1000

    Fix navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,7 @@ const links = [
 
 export default function Navbar() {
   return (
-    <div className="fixed w-full bottom-0 flex justify-center gap-2 mx-auto px-2 py-4 z-20">
+    <div className="fixed w-max left-1/2 -translate-x-1/2 bottom-0 flex justify-center gap-2 mx-auto px-2 py-4 z-20">
       <div className="flex h-12 p-1 shadow-lg bg-secondary/80 backdrop-blur-md rounded-full [&>*]:my-auto">
         {links.map(({ name, href }) => (
           <Link key={name} href={href} className="flex flex-1 h-full hover:bg-background transition w-20 rounded-full"><span className="text-center w-full my-auto">{name}</span></Link>
