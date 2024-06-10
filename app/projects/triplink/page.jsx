adae959a03d56738138cd416db3bf0fdefaaff39
Michael Ren <michaelren0129@gmail.com>
2024-06-11 00:53:52 +1000

    Add images for triplink

--- app/projects/triplink/page.jsx
+++ app/projects/triplink/page.jsx
@@ -17,11 +17,19 @@ const cards = [
   { name: 'Poster', image: require('@/public/triplink/poster4.png'), orientation: 'portrait', date: '2022' },
   { name: 'Poster', image: require('@/public/triplink/poster5.png'), orientation: 'portrait', date: '2022' },
   { name: 'Poster', image: require('@/public/triplink/poster6.png'), orientation: 'portrait', date: '2022' },
+  { name: 'Poster', image: require('@/public/triplink/poster-practice.png'), orientation: 'portrait', date: '2022' },
   { name: 'Insta Post', image: require('@/public/triplink/insta1.png'), orientation: 'portrait', date: '2022' },
   { name: 'Insta Post', image: require('@/public/triplink/insta2.png'), orientation: 'portrait', date: '2022' },
   { name: 'Insta Post', image: require('@/public/triplink/insta3.png'), orientation: 'portrait', date: '2022' },
   { name: 'Insta Post', image: require('@/public/triplink/insta4.png'), orientation: 'portrait', date: '2022' },
-  { name: 'Double Page Spread', image: require('@/public/triplink/dps.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Double Page Spread', image: require('@/public/triplink/dps1.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Double Page Spread', image: require('@/public/triplink/dps2.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Double Page Spread', image: require('@/public/triplink/dps3.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Double Page Spread', image: require('@/public/triplink/dps4.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Double Page Spread Mockup', image: require('@/public/triplink/dps-mockup.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Website Mockup', image: require('@/public/triplink/website-mockup.png'), orientation: 'landscape', date: '2022' },
+  { name: 'Website Mockup', image: require('@/public/triplink/website-mockup-mobile.png'), orientation: 'landscape', date: '2022' },
+  { name: 'App', image: require('@/public/triplink/app.png'), orientation: 'landscape', date: '2022' },
   ]
   
   const boards = [
